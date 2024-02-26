24d134adb200d2e647eb383c23413285fce96202
vospi97 <vospi97@tutanota.com>
2024-02-26 18:17:05 -0500

    creación de primer trabajo de programción para la industria

---
+++ clase 23 febrero prog industria.py
@@ -0,0 +1,12 @@
+ def suma(a: int, b:int) -> int:
+    """Esta función calcula la suma de dos números enteros 
+
+    Argas:
+    a (int): número entero
+    b (int): número entero
+
+    """
+
+    c: int = a + b 
+    return c
+
